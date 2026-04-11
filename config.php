64acf3bf4957e3bedb731547a15f306e96f9a861
Issa Sukatu Abdullahi <sukaissa@gmail.com>
2026-04-11 09:47:22 +0000

    Merge corrections with dev

--- config.php
+++ config.php
@@ -6,9 +6,9 @@ if (session_status() === PHP_SESSION_NONE) {
 
 // Database configuration
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'Hausawi@2025');
-define('DB_NAME', 'cgs');
+define('DB_USER', 'rightgh24_cgs');
+define('DB_PASS', '86SwzU5UGh');
+define('DB_NAME', 'rightgh24_cgs');
 
 // Create database connection
 function getDBConnection() {
